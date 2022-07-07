912ecd19b5c58c9a0c03587add25b7da92abd889
Simon Nitsche <s2110438010@fhooe.at>
2022-07-07 18:29:14 +0200

    Added Model InternalProcesses.cs

---
+++ Task-Manager/InternalProcesses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager
+{
+    internal class InternalProcesses
+    {
+
+        #region Members
+        List<Process> _internalProcesses;
+        #endregion
+
+        #region Properties
+        public List<Process> InternalProcesses
+        {
+            get { return _internalProcesses; }
+        }
+        #endregion
+
+
+        #region Construktor
+        public ProcessManager()
+        {
+            _internalProcesses = Process.GetProcesses().ToList();
+        }
+        #endregion
+
+
+        #region Methods
+
+        public void RefreshProcesses()
+        {
+            _internalProcesses = Process.GetProcesses().ToList();
+        }
+
+        public int ReturnActualNumberOfProcesses()
+        {
+            RefreshProcesses();
+            return _internalProcesses.Count;
+        }
+
+        #endregion
+
+    }
+}
--- Task-Manager/ProcessManager.cs
+++ Task-Manager/ProcessManager.cs
@@ -9,40 +9,7 @@ namespace Task_Manager
 {
     internal class ProcessManager
     {
-        #region Members
-        List<Process> _internalProcesses;
-        #endregion
-
-        #region Properties
-        public List<Process> InternalProcesses
-        {
-            get { return _internalProcesses; }
-        }
-        #endregion
-
-
-        #region Construktor
-        public ProcessManager()
-        {
-            _internalProcesses = Process.GetProcesses().ToList();
-        }
-        #endregion
-
-
-        #region Methods
-
-        public void RefreshProcesses()
-        {
-            _internalProcesses = Process.GetProcesses().ToList();
-        }
-
-        public int ReturnActualNumberOfProcesses()
-        { 
-            RefreshProcesses();
-            return _internalProcesses.Count;
-        }
-
-        #endregion
+       
 
     }
 }
